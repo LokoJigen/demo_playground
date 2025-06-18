e787b602f45faa8093c6f0837fb1d2c9c5f502d7
GinoV <ginovalerio123@gmail.com>
2025-06-18 14:54:30 +0200

    modified BlendShapesSO.cs - added duration

--- Assets/_Scripts/ScriptableObjects/BlendShapeSO.cs
+++ Assets/_Scripts/ScriptableObjects/BlendShapeSO.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BlendShapeSO", menuName = "SO/BlendShapeSO")]
 public class BlendShapeSO : ScriptableObject
 {
+    public float blendShapeDuration = 1f;
     public List<float> blendShapeDatas = new List<float>();
 
 }
